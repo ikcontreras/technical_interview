e188dceb93992afec0191dd61706192ed6dc5f71
ikcontreras <ikcontreras@outlook.es>
2025-03-16 20:48:35 +0100

    feat(discount): create discount for SKUs ending in 5

---
+++ src/main/java/com/capitole/technical_interview/domain/discount/SkuDiscountRule.java
@@ -0,0 +1,18 @@
+package com.capitole.technical_interview.domain.discount;
+
+import com.capitole.technical_interview.domain.model.Product;
+
+import java.math.BigDecimal;
+
+public class SkuDiscountRule implements DiscountRule {
+
+    private static final BigDecimal DISCOUNT = new BigDecimal("0.30");
+
+    @Override
+    public void applyDiscount(Product product) {
+        if (product.getSku().endsWith("5")) {
+            BigDecimal discountAmount = product.getPrice().multiply(DISCOUNT);
+            product.setPrice(product.getPrice().subtract(discountAmount));
+        }
+    }
+}
---
+++ src/test/java/com/capitole/technical_interview/domain/discount/SkuDiscountRuleTest.java
@@ -0,0 +1,26 @@
+package com.capitole.technical_interview.domain.discount;
+
+import com.capitole.technical_interview.domain.model.Product;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SkuDiscountRuleTest {
+    private final SkuDiscountRule discountRule = new SkuDiscountRule();
+
+    @Test
+    void shouldApply30PercentDiscountForSkuEndingIn5() {
+        Product product = new Product("SKU0005", new BigDecimal("120.00"), "Noise-Cancelling Headphones", "Electronics");
+        discountRule.applyDiscount(product);
+        assertEquals(new BigDecimal("84.00"), product.getPrice());
+    }
+
+    @Test
+    void shouldNotApplyDiscountForOtherSkus() {
+        Product product = new Product("SKU0002", new BigDecimal("500.00"), "4K Smart TV", "Electronics");
+        discountRule.applyDiscount(product);
+        assertEquals(new BigDecimal("500.00"), product.getPrice());
+    }
+}
