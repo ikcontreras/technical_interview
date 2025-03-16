beb1ccf1f4aa642c8f5a853a979fdf6d7b01cce9
ikcontreras <ikcontreras@outlook.es>
2025-03-16 19:04:59 +0100

    Start project

---
+++ src/test/java/com/capitole/technical_interview/TechnicalInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitole.technical_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
