6c0e508d60dcaa3da8cb253db5e161f0c0b4a49a
ikcontreras <ikcontreras@outlook.es>
2025-03-16 22:21:57 +0100

    feat(filters): products can be sorted

---
+++ src/main/java/com/capitole/technical_interview/domain/service/SortProducts.java
@@ -0,0 +1,27 @@
+package com.capitole.technical_interview.domain.service;
+
+import com.capitole.technical_interview.domain.model.Product;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortProducts {
+    public List<Product> sort(List<Product> products, String column, boolean ascending) {
+        Comparator<Product> comparator = switch (column.toLowerCase()) {
+            case "sku" -> Comparator.comparing(Product::getSku);
+            case "price" -> Comparator.comparing(Product::getPrice);
+            case "description" -> Comparator.comparing(Product::getDescription);
+            case "category" -> Comparator.comparing(Product::getCategory);
+            default -> throw new IllegalArgumentException("Invalid sort field: " + column);
+        };
+
+        if (!ascending) {
+            comparator = comparator.reversed();
+        }
+
+        return products.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+    }
+}
