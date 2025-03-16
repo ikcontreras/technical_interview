054c83de5f73cf0715b2d64d4271c09f4b748a33
ikcontreras <ikcontreras@outlook.es>
2025-03-16 19:30:07 +0100

    chore: load test products

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS products (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    sku VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    category VARCHAR(100) NOT NULL
+);
