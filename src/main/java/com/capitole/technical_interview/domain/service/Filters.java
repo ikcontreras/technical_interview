e1819cafaedfa15fa77dbe1bb0b4b8f67a403152
ikcontreras <ikcontreras@outlook.es>
2025-03-16 23:47:32 +0100

    fix: annotate class with @Component for dependency injection

--- src/main/java/com/capitole/technical_interview/domain/service/Filters.java
+++ src/main/java/com/capitole/technical_interview/domain/service/Filters.java
@@ -1,10 +1,12 @@
 package com.capitole.technical_interview.domain.service;
 
 import com.capitole.technical_interview.domain.model.Product;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class Filters {
     public List<Product> filterByCategory(List<Product> products, String category) {
         return products.stream()
--- src/main/java/com/capitole/technical_interview/domain/service/SortProducts.java
+++ src/main/java/com/capitole/technical_interview/domain/service/SortProducts.java
@@ -1,11 +1,13 @@
 package com.capitole.technical_interview.domain.service;
 
 import com.capitole.technical_interview.domain.model.Product;
+import org.springframework.stereotype.Component;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class SortProducts {
     public List<Product> sort(List<Product> products, String column, boolean ascending) {
         Comparator<Product> comparator = switch (column.toLowerCase()) {
