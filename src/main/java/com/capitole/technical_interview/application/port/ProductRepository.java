0ebc3b41aa06b1420f14f4907d9736012657e808
ikcontreras <ikcontreras@outlook.es>
2025-03-17 00:07:48 +0100

    refactor: remove unused methods

--- src/main/java/com/capitole/technical_interview/application/port/ProductRepository.java
+++ src/main/java/com/capitole/technical_interview/application/port/ProductRepository.java
@@ -6,6 +6,4 @@ import java.util.List;
 
 public interface ProductRepository {
     List<Product> findAll();
-    List<Product> findByCategory(String category);
-    List<Product> findAllSorted(String sortBy, boolean ascending);
 }
