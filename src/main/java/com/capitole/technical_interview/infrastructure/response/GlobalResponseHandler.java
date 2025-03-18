ca202d66109f97ef87e0765e67bfe64f9fa0883f
ikcontreras <ikcontreras@outlook.es>
2025-03-19 00:24:08 +0100

    fix: exclude Swagger and OpenAPI requests from response handler

--- src/main/java/com/capitole/technical_interview/infrastructure/response/GlobalResponseHandler.java
+++ src/main/java/com/capitole/technical_interview/infrastructure/response/GlobalResponseHandler.java
@@ -9,8 +9,14 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+import java.util.List;
+
 @ControllerAdvice
 public class GlobalResponseHandler implements ResponseBodyAdvice<Object> {
+
+    private static final List<String> EXCLUDED_PATHS = List.of(
+            "/swagger-ui", "/swagger-ui.html", "/v3/api-docs", "/v3/api-docs.yaml", "/swagger-resources"
+    );
 
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
@@ -21,6 +27,12 @@ public class GlobalResponseHandler implements ResponseBodyAdvice<Object> {
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
       Class<? extends HttpMessageConverter<?>> selectedConverterType,
       ServerHttpRequest request, ServerHttpResponse response) {
+
+        String requestPath = request.getURI().getPath();
+
+        if (EXCLUDED_PATHS.stream().anyMatch(requestPath::contains)) {
+            return body;
+        }
 
         if (body instanceof ErrorResponse apiResponse) {
             return apiResponse;
